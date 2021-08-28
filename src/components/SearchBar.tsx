f2e63b5610c65506dc4d75efb37876dd040a8d02
md2eoseo <md2eoseo@gmail.com>
2021-08-28 13:40:18 +0900

    emotion -> styled-components

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,4 +1,4 @@
-import styled from '@emotion/styled';
+import styled from 'styled-components';
 import { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import logo from '../logo.svg';
