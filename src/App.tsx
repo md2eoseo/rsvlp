0dde98bdd7d11a38b4e95b8a50e04fe9503d4d28
md2eoseo <md2eoseo@gmail.com>
2021-09-06 11:47:09 +0900

    페이지 전환 애니메이션(fade) 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Main from './pages/Main';
 import Search from './pages/Search';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import './App.css';
 
 function App() {
   return (
     <div style={{ backgroundColor: '#282c34' }}>
       <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={Main} />
-          <Route path="/search" component={Search} />
-        </Switch>
+        <Route
+          render={({ location }) => (
+            <TransitionGroup>
+              <CSSTransition key={location.pathname} timeout={1000} classNames="fade">
+                <Switch location={location}>
+                  <Route exact path="/" component={Main} />
+                  <Route path="/search" component={Search} />
+                </Switch>
+              </CSSTransition>
+            </TransitionGroup>
+          )}
+        />
       </BrowserRouter>
     </div>
   );
